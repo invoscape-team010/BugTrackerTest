2121886a9ba8b2d6d8560b966cdabc5b30fc77b0
invoscape-team010 <team010@invoscape.com>
2014-08-08 16:51:28 +0530

    User updation done

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ BugTrackerTest::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
     resources :bugs
+    resources :users
 
   # Sample resource route with options:
   #   resources :products do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140808100700) do
+ActiveRecord::Schema.define(:version => 20140808111312) do
 
   create_table "bugs", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(:version => 20140808100700) do
     t.string   "priority"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
